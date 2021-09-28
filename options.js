0a85054f6fd28038e2ea19e31079c4bacd42a81b
Richard Wohlbold <richard@rgwohlbold.de>
2021-09-28 19:26:54 +0200

    Remove form

--- options.js
+++ options.js
@@ -93,21 +93,21 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-document.querySelector("#itemForm").addEventListener("submit", event => {
-    event.preventDefault();
-    const name = document.querySelector("#itemName").value;
-    const link = document.querySelector("#itemLink").value;
-    const price = document.querySelector("#itemPrice").value;
-    const image = document.querySelector("#itemImage").value;
-    browser.storage.sync.get("items").then(result => {
-        let items = result.items;
-        if (!items) {
-            items = [];
-        }
-        items.push({name, link, price, image, timeStamp: Date.now()});
-        console.log("newItems", items)
-        browser.storage.sync.set({items});
-        location.reload();
-    });
-});
+// document.querySelector("#itemForm").addEventListener("submit", event => {
+//     event.preventDefault();
+//     const name = document.querySelector("#itemName").value;
+//     const link = document.querySelector("#itemLink").value;
+//     const price = document.querySelector("#itemPrice").value;
+//     const image = document.querySelector("#itemImage").value;
+//     browser.storage.sync.get("items").then(result => {
+//         let items = result.items;
+//         if (!items) {
+//             items = [];
+//         }
+//         items.push({name, link, price, image, timeStamp: Date.now()});
+//         console.log("newItems", items)
+//         browser.storage.sync.set({items});
+//         location.reload();
+//     });
+// });
 
