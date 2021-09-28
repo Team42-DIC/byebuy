c26aa4380cf5a3dd1cec09da88258b19dacdf01e
Alexander Sohn <github@asohn.de>
2021-09-28 17:54:29 +0200

    Add button and start with storage

--- buybye.js
+++ buybye.js
@@ -1,16 +1,34 @@
-
 function onError(error) {
   console.log(`Error: ${error}`);
 }
+var newNode = document.createElement("div");
+newNode.className = "a-button a-button-stack a-spacing-small a-button-inner celwidget a-button-icon ";
+newNode.textContent = "Buy me later!";
 
-function onGot(item) {
-  let color = "blue";
-  if (item.color) {
-    color = item.color;
-  }
-  document.body.style.border = "10px solid " + color;
-}
+var cartButton = document.getElementById("addToCart_feature_div");
+cartButton.parentNode.insertBefore(newNode, cartButton.nextSibling);
 
-let getting = browser.storage.sync.get("color");
-getting.then(onGot, onError);
+const link = window.location.href;
+const title = document.getElementById("productTitle").innerText;
+const price = document.getElementById("price_inside_buybox").innerText;
+const imageLink = document.getElementById("landingImage").src;
+const timeStamp = Date.now();
+browser.storage.local.get()
+console.log(timeStamp)
+    console.log("teste")
+    browser.storage.sync.get("items").then(result => {
+        console.log(result)
+    }, onError);
+/* browser.storage.sync.get("items").then(result => {
+    console.log("Trolol")
+ let items = result.items;
+ if (!items) {
+  items = [];
+ }
+ items.push({ title, link, price, imageLink, timeStamp });
+ console.log("newItems", items)
+ browser.storage.sync.set({ items });
+}, onError);
 
+console.log("test")
+browser.storage.sync.get("items").then(console.log) */
