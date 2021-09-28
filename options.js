87c524c02cba46f837506f380e2b73540fda74e2
Richard Wohlbold <richard@rgwohlbold.de>
2021-09-28 17:35:06 +0200

    Add items to options page

--- options.js
+++ options.js
@@ -1,24 +1,32 @@
-function saveOptions(e) {
-  e.preventDefault();
-  browser.storage.sync.set({
-    color: document.querySelector("#color").value
-  });
-}
+document.addEventListener("DOMContentLoaded", () => {
+    browser.storage.sync.get("items").then(result => {
+        const itemRoot = document.querySelector('#itemRoot')
+        for (const item in result.items) {
+            console.log("item", result.items[item])
+            
+            const itemDiv = document.createElement("div");
+            const itemLink = document.createElement("a");
+            itemLink.href = result.items[item].link;
+            itemLink.target = "_blank";
+            itemLink.innerHTML = result.items[item].name;
+            itemDiv.appendChild(itemLink);
+            itemRoot.appendChild(itemDiv)
+        }
+    });
+});
 
-function restoreOptions() {
-
-  function setCurrentChoice(result) {
-    document.querySelector("#color").value = result.color || "blue";
-  }
-
-  function onError(error) {
-    console.log(`Error: ${error}`);
-  }
-
-  let getting = browser.storage.sync.get("color");
-  getting.then(setCurrentChoice, onError);
-}
-
-document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+document.querySelector("#itemForm").addEventListener("submit", event => {
+    event.preventDefault();
+    const name = document.querySelector("#itemName").value;
+    const link = document.querySelector("#itemLink").value;
+    browser.storage.sync.get("items").then(result => {
+        let items = result.items;
+        if (!items) {
+            items = [];
+        }
+        items.push({name, link});
+        console.log("newItems", items)
+        browser.storage.sync.set({items});
+    });
+});
 
